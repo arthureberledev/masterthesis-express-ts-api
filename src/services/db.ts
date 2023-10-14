67a0b847f1e66562e7ce429ceea4dda1c0cae960
arthureberle <58264696+arthureberle@users.noreply.github.com>
2023-10-14 11:40:52 +0200

    set different pool config

--- src/services/db.ts
+++ src/services/db.ts
@@ -20,6 +20,29 @@ export function getDbPool() {
       user: process.env.DB_USER,
       database: process.env.DB_NAME,
       password: process.env.DB_PASS,
+      /**
+       * Determines the pool's action when no connections are available and the limit has been reached.
+       * If true, the pool will queue the connection request and call it when one becomes available. If false, the pool will immediately call back with an error.
+       */
+      waitForConnections: true,
+      /**
+       * The maximum number of connections to create at once.
+       */
+      connectionLimit: 100,
+      /**
+       * The maximum number of idle connections. (Default: same as connectionLimit)
+       */
+      maxIdle: 50,
+      /**
+       * The idle connections timeout, in milliseconds.
+       */
+      idleTimeout: 30000,
+      queueLimit: 0,
+      enableKeepAlive: true,
+      /**
+       * If keep-alive is enabled users can supply an initial delay.
+       */
+      keepAliveInitialDelay: 100,
     });
   }
   return pool.promise();
